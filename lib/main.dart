9ee90b7e383c617f201743462d31b6c224c044e3
jinbum9958 <jinbum9958@gmail.com>
2023-05-22 13:09:58 +0900

    add .gitignore

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:likelion_project/common/view/splash_screen.dart';
+
+void main() => runApp(_App());
+
+class _App extends StatelessWidget {
+  const _App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'NotoSans',
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
+    );
+  }
+}
